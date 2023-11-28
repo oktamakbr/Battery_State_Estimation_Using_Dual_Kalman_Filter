f06a0d98b4637409f7721c96dc6788b9418fec71
Muhammad Oktama <134263938+oktamakbr@users.noreply.github.com>
2023-11-28 21:18:26 +0700

    Add files via upload

---
+++ coretan.m
@@ -0,0 +1,8 @@
+clear, clc
+
+oldopts = optimset('TolFun',1e-6);
+options = optimset(oldopts,'PlotFcns','optimplotfval','TolX',1e-7);
+
+fun = @(x)x^2; % Rosenbrock's function
+x0 = [9];
+[x,fval] = fminsearch(fun,x0,options)
