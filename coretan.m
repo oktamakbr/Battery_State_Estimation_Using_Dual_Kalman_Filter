317c245a9d786f602259a4d048fecc48a08212ad
Muhammad Oktama <134263938+oktamakbr@users.noreply.github.com>
2023-12-01 08:43:39 +0700

    Update coretan.m

--- coretan.m
+++ coretan.m
@@ -1,11 +1,1 @@
 clear, clc
-
-oldopts = optimset('TolFun',1e-6);
-options = optimset(oldopts,'PlotFcns','optimplotfval','TolX',1e-7);
-
-fun = @(x)x^2; % Rosenbrock's function
-x0 = [9];
-[x,fval] = fminsearch(fun,x0,options)
-
-x = [1 2 3];
-
